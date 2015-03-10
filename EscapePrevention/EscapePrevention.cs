70441d5ea1db98c9e405f94deac57995cb8e8cef
Mark Greenway <mgreenway@gmail.com>
2015-03-10 02:20:37 -0400

    Nuget start

---
+++ EscapePrevention/EscapePrevention.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapePrevention
+{
+    public static class EscapePrevention 
+    {
+        public static string EscapePrevent(this string input, EscapePreventionKind kind = EscapePreventionKind.UrlSpaceRemoval)
+        {
+            if (String.IsNullOrEmpty(input))
+            {
+                return String.Empty;
+            }
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                return String.Empty;
+            }
+            return input;
+        }
+    }
+}
---
+++ EscapePrevention/EscapePreventionKind.cs
@@ -0,0 +1,9 @@
+namespace EscapePrevention
+{
+    public enum EscapePreventionKind
+    {
+        UrlSpaceRemoval,
+        UrlSpaceReplacement,
+        XML
+    }
+}
