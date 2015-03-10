57a18c4c9382a950d5fe08a721e0751efd9c7f26
Mark Greenway <mgreenway@gmail.com>
2015-03-10 02:56:23 -0400

    Smoke

---
+++ EscapePrevention-Tests/Smoke.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EscapePrevention_Tests
+{
+    [TestFixture]
+    public class Smoke
+    {
+        [Test]
+        public void MathTest()
+        {
+            Assert.AreEqual(2, 1 + 1);
+        }
+    }
+}
